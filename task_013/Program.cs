30080e192dbb2e017dad4aa16152219e3117bf07
alenapech <alenaryzhova@inbox.ru>
2022-06-15 20:27:02 +0400

    add two tasks

---
+++ task_013/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+string[] array = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.WriteLine("Ввведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 0 && n < 5)
+{
+ Console.WriteLine($"{array[n]} является рабочим днём");  
+ 
+}
+else if (n >= 5 && n < 7)
+{
+   Console.WriteLine($"{array[n]} является выходным днём");  
+
+}
+else
+{
+   Console.WriteLine("Отсутствует такой день недели");   
+}
+
---
+++ task_015/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Ввведите число: ");
+string n = Console.ReadLine();
+if (n.Length < 3)
+{
+Console.WriteLine("В заданном числе нет третьей цифры");
+}
+else{
+    Console.WriteLine(n[2]);
+   //char[] array;
+// array = n.ToCharArray(2,1);
+//Console.WriteLine(array);
+}
+
